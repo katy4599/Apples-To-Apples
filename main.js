74fe693f5b066f070787f3bc6fa3cf6328ca78b2
katy millard <katy.millard@gmail.com>
2022-02-02 10:22:33 -0600

    Second commit

--- main.js
+++ main.js
@@ -3,13 +3,18 @@ import input from 'input';
 async function main() {
     const drivingAge = 18
     const myAge = await input.text('What is your Age?');
+    const n = (drivingAge - myAge);
 
-    if (myAge >= drivingAge) {
-        console.log('You can drive!')
-    } else {
-        console.log('Sorry Loser!')
-    }
-    
-}
+    if (myAge > drivingAge) {
+        const hasLicense = await input.confirm('Do you have your license?')
+        if (myAge < drivingAge || !hasLicense) {
+            console.log(`Wait ${ n } years!`)
+        } if (myAge >= drivingAge && !hasLicense) {
+            console.log('Sorry Loser!')   
+        } if (myAge >= drivingAge && hasLicense) {
+            console.log('You can drive!')
+        }
+    }   
+}   
 
 main()

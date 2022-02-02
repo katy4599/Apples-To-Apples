601ab0f39905c518a53d9c14ccf1a5bbb7fa1754
katy millard <katy.millard@gmail.com>
2022-02-02 13:32:49 -0600

    Breaking the while loop

--- main.js
+++ main.js
@@ -2,23 +2,29 @@ import input from 'input';
 
 async function main() {
     const drivingAge = 18
+    let toContinue = true;
     
-    while (true) {
+    while (toContinue) {
         const myAge = await input.text('What is your Age?');
 
-        if (myAge >= drivingAge) {
-        const hasLicense = await input.confirm('Do you have your license?')
-        const n = (drivingAge - myAge);
-        
-            if (myAge >= drivingAge && !hasLicense) {
-                console.log('Go get your license!')   
-            } else if (myAge >= drivingAge && hasLicense) {
-                console.log('You can drive!')
+            if (myAge >= drivingAge) {
+            const hasLicense = await input.confirm('Do you have your license?')
+            
+                if (!hasLicense) {
+                    console.log('Go get your license!')   
+                } else {
+                    console.log('You can drive!')
+                } 
+            } else  {
+                const n = (drivingAge - myAge);
+                console.log(`Wait ${ n } years!`)
             } 
-        else {
-            console.log(`Wait ${ n } years!`)
+        
+        let toContinue = await input.confirm('Do you want to continue?');
+    
+            if (toContinue == false) {
+              break;
             }
-        }
     }
 }   
 

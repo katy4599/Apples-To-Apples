f3bc427c5aa96ee6c2e13f79d6f45b0340582c62
katy millard <katy.millard@gmail.com>
2022-02-02 15:02:12 -0600

    Shopping List

--- main.js
+++ main.js
@@ -1,31 +1,22 @@
 import input from 'input';
 
 async function main() {
-    const drivingAge = 18
-    let toContinue = true;
+    const shopping = [];
     
-    while (toContinue) {
-        const myAge = await input.text('What is your Age?');
-
-            if (myAge >= drivingAge) {
-            const hasLicense = await input.confirm('Do you have your license?')
-            
-                if (!hasLicense) {
-                    console.log('Go get your license!')   
-                } else {
-                    console.log('You can drive!')
-                } 
-            } else  {
-                const n = (drivingAge - myAge);
-                console.log(`Wait ${ n } years!`)
-            } 
+    let finished = false;
+    
+    while (!finished) {
+        const resp = await input.text('Enter an item (or "finished")');
         
-        let toContinue = await input.confirm('Do you want to continue?');
+        if (resp == 'finished') {
+            finished = true     
+        } else {
+            shopping.push(resp)
+        }
+    } 
+    console.log('Here is your shopping list!');
+    console.log(shopping);
     
-            if (toContinue == false) {
-              break;
-            }
-    }
-}   
+}
 
 main()

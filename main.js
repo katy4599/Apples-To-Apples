073f737d0e4add677026f7467a889465987f0020
katy millard <katy.millard@gmail.com>
2022-02-02 11:30:16 -0600

    Corrected while loop

--- main.js
+++ main.js
@@ -2,18 +2,22 @@ import input from 'input';
 
 async function main() {
     const drivingAge = 18
-    const myAge = await input.text('What is your Age?');
-    const n = (drivingAge - myAge);
+    
+    while (true) {
+        const myAge = await input.text('What is your Age?');
 
-    while (myAge > drivingAge) {
+        if (myAge >= drivingAge) {
         const hasLicense = await input.confirm('Do you have your license?')
+        const n = (drivingAge - myAge);
         
-        if (myAge >= drivingAge && !hasLicense) {
-            console.log('Go get your license!')   
-        } else if (myAge >= drivingAge && hasLicense) {
-            console.log('You can drive!')
-        } else {
-        console.log(`Wait ${ n } years!`)
+            if (myAge >= drivingAge && !hasLicense) {
+                console.log('Go get your license!')   
+            } else if (myAge >= drivingAge && hasLicense) {
+                console.log('You can drive!')
+            } 
+        else {
+            console.log(`Wait ${ n } years!`)
+            }
         }
     }
 }   
